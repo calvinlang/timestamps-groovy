b0c2298a525f19b10578ee8b48944a032d7e74c1
Calvin Lang <langcalvin@gmail.com>
2016-03-13 16:30:17 -0500

    Admin and User roles created in the bootstrap, server runs

--- TimeStampsGroovy/grails-app/conf/BootStrap.groovy
+++ TimeStampsGroovy/grails-app/conf/BootStrap.groovy
@@ -1,6 +1,13 @@
+import com.timestamps.auth.*
 class BootStrap {
 
     def init = { servletContext ->
+    	def adminRole = Role.findOrSaveWhere(authority: 'ROLE_ADMIN')
+    	def user = User.findOrSaveWhere(username: 'calvin', password: 'password')
+
+    	if(!user.authorities.contains(adminRole)) {
+    		UserRole.create(user, adminRole, true)
+    	}
     }
     def destroy = {
     }
